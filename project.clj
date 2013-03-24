99a6852af3f75ef5e6abccd16d9e3378da658bcc
ujihisa <ujihisa@gmail.com>
2013-03-23 18:01:25 -0700

    initial import that postgres stuff works!

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject postgres-lingrbot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "Use PostgreSQL from Lingr"
+  :url "http://github.com/ujihisa/postgres-lingrbot"
+  :license {:name "GNU GPL v3+"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [org.clojars.ccfontes/korma "0.3.0-beta12-pgssl"]]
+  :main postgres-lingrbot.core)
