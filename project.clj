1c676c11c936e16cd79ce0311154033e86d9c102
ujihisa <ujihisa@gmail.com>
2013-04-02 18:28:55 -0700

    hmm?

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/ujihisa/postgres-lingrbot"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql "9.1-901.jdbc4"]
@@ -11,5 +11,5 @@
                  [ring/ring-jetty-adapter "1.1.6"]
                  [compojure "1.1.3"]
                  [leiningen-core "2.0.0"]]
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.1.0"
   :main postgres-lingrbot.core)
